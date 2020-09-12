8cc26fdbabdf7b9755450f8d3aca6eef38e95abb
unknown <dskinner_93@yahoo.com>
2020-09-11 19:52:57 -0700

    Completed Sprint Challenge - No Stretch

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />)
+    const Header = getByText(/Checkout Form/i)
+    expect(Header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByTestId } = render(<CheckoutForm />);
+
+    const submitButton = getByTestId('successButton');
+
+    expect(submitButton).toBeInTheDocument();
+
+    fireEvent.click(submitButton);
+
+    const success = getByTestId('successMessage');
+
+    expect(success).toBeInTheDocument();
+});
